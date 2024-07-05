3bb489abee76443b9c67eecfa2e05e52d8342c23
Andres Martinez <andres.martinez04@globant.com>
2024-07-05 17:37:53 -0300

    Export Blog entity and added columns

--- src/blogs/blogs.module.ts
+++ src/blogs/blogs.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
-import { BlogsService } from './blogs.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { BlogsResolver } from './blogs.resolver';
+import { BlogsService } from './blogs.service';
+import { Blog } from './entities/blog.entity';
 
 @Module({
   providers: [BlogsResolver, BlogsService],
+  imports: [TypeOrmModule.forFeature([Blog])],
 })
 export class BlogsModule {}
--- src/blogs/entities/blog.entity.ts
+++ src/blogs/entities/blog.entity.ts
@@ -1,19 +1,25 @@
 import { Field, ID, ObjectType } from '@nestjs/graphql';
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 import { BlogTags } from '../enums/tags.enums';
 
 @Entity({ name: 'blogs' })
 @ObjectType()
 export class Blog {
   @Field(() => ID)
+  @PrimaryGeneratedColumn('uuid')
   id: string;
 
   @Field(() => String)
+  @Column('text')
   title: string;
 
   @Field(() => String)
+  @Column('text')
   body: string;
 
   @Field(() => [BlogTags])
+  @Column('text', {
+    array: true,
+  })
   tags: BlogTags[];
 }
