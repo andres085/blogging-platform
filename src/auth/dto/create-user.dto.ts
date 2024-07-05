ceb9e1f8dcf25fc451cb0dc285a45b9e9972d436
Andres Martinez <andres.martinez04@globant.com>
2024-07-05 13:05:22 -0300

    Adds dto and user register

---
+++ src/auth/dto/create-user.dto.ts
@@ -0,0 +1,24 @@
+import {
+  IsEmail,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
+
+export class CreateUserDto {
+  @IsEmail()
+  email: string;
+
+  @IsString()
+  @MinLength(3, { message: 'Password must be greater than 3 characters.' })
+  @MaxLength(10, { message: 'Password must be less than 10 characters.' })
+  @Matches(/(?:(?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/, {
+    message:
+      'The password must have a Uppercase, lowercase letter and a number',
+  })
+  password: string;
+
+  @IsString()
+  fullName: string;
+}
