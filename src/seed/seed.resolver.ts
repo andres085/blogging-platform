df439fa378e64a0d41dff1a7a499b71a81066558
Andres Martinez <andres.martinez04@globant.com>
2024-07-08 15:19:07 -0300

    Adds authorization to execute the seeding of the database

--- src/seed/seed.resolver.ts
+++ src/seed/seed.resolver.ts
@@ -1,7 +1,10 @@
+import { UseGuards } from '@nestjs/common';
 import { Mutation, Resolver } from '@nestjs/graphql';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
 import { SeedService } from './seed.service';
 
 @Resolver()
+@UseGuards(JwtAuthGuard)
 export class SeedResolver {
   constructor(private readonly seedService: SeedService) {}
 
