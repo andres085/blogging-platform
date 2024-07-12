d590bd03305e2fb15ec70179865878f3a4a9d81e
Andres Martinez <andres.martinez04@globant.com>
2024-07-12 17:48:03 -0300

    Add method to search all blogs in the admin panel resolver

--- src/admin-panel/admin-panel.module.ts
+++ src/admin-panel/admin-panel.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { BlogsModule } from '../blogs/blogs.module';
 import { UsersModule } from '../users/users.module';
 import { AdminPanelResolver } from './admin-panel.resolver';
 import { AdminPanelService } from './admin-panel.service';
 
 @Module({
   providers: [AdminPanelResolver, AdminPanelService],
-  imports: [UsersModule],
+  imports: [UsersModule, BlogsModule],
 })
 export class AdminPanelModule {}
--- src/admin-panel/admin-panel.resolver.ts
+++ src/admin-panel/admin-panel.resolver.ts
@@ -1,6 +1,9 @@
 import { UseGuards } from '@nestjs/common';
-import { Query, Resolver } from '@nestjs/graphql';
+import { Args, Query, Resolver } from '@nestjs/graphql';
 import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { BlogsService } from '../blogs/blogs.service';
+import { SearchByTagArg } from '../blogs/dto/args/search.args';
+import { Blog } from '../blogs/entities/blog.entity';
 import { CurrentUser } from '../common/decorators/current-user.decorator';
 import { ValidRoles } from '../common/enums/valid-roles.enum';
 import { User } from '../users/entities/user.entity';
@@ -13,10 +16,21 @@ export class AdminPanelResolver {
   constructor(
     private readonly adminPanelService: AdminPanelService,
     private readonly usersService: UsersService,
+    private readonly blogsService: BlogsService,
   ) {}
 
   @Query(() => [User], { name: 'getAllUsers' })
-  async findAll(@CurrentUser([ValidRoles.admin]) _: User): Promise<User[]> {
+  async findAllUsers(
+    @CurrentUser([ValidRoles.admin]) _: User,
+  ): Promise<User[]> {
     return this.usersService.findAll();
+  }
+
+  @Query(() => [User], { name: 'getAllUsers' })
+  async findAllBlogs(
+    @CurrentUser([ValidRoles.admin]) _: User,
+    @Args() searchByTagArg: SearchByTagArg,
+  ): Promise<Blog[]> {
+    return this.blogsService.findAll(searchByTagArg);
   }
 }
