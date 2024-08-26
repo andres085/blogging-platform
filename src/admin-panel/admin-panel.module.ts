e24b37d0299e6fbf88d9f989141891d2c213c9c2
Andres Martinez <andres.martinez04@globant.com>
2024-08-26 11:05:35 -0300

    add flag search by status or entity type

--- src/admin-panel/admin-panel.module.ts
+++ src/admin-panel/admin-panel.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { BlogsModule } from '../blogs/blogs.module';
+import { FlagsModule } from '../flags/flags.module';
 import { UsersModule } from '../users/users.module';
 import { AdminPanelResolver } from './admin-panel.resolver';
 import { AdminPanelService } from './admin-panel.service';
 
 @Module({
   providers: [AdminPanelResolver, AdminPanelService],
-  imports: [UsersModule, BlogsModule],
+  imports: [UsersModule, BlogsModule, FlagsModule],
 })
 export class AdminPanelModule {}
